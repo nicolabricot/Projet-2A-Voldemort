000166a6f0a8df13b1eb11dc0b01a8748130d741
nicolabricot <github@nicolabricot.com>
2013-06-12 16:13:57 +0200

    Add static URL for webpages

--- src/main/java/fr/uha/projetvoldemort/webservice/ServiceMap.java
+++ src/main/java/fr/uha/projetvoldemort/webservice/ServiceMap.java
@@ -5,6 +5,7 @@
 package fr.uha.projetvoldemort.webservice;
 
 import fr.uha.projetvoldemort.fight.FightDemo;
+import fr.uha.projetvoldemort.resource.Resources;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -31,12 +32,11 @@ public class ServiceMap {
     @GET
     @Path("/states/{id_character}")
     public Response getStates() {
-        String url = "./map.jsp?map=";
         StringBuilder sb = new StringBuilder();
         sb.append("{");
-        sb.append("\"champagne-ardenne\": [{\"type\": \"opened\", \"title\": \"Champagne-Ardenne\", \"link\":\"" + url + "champagne-ardenne\", \"description\": \"Want to drink a glass of Champagne?\"}],");
-        sb.append("\"alsace\": [{\"type\": \"opened\", \"title\": \"Alsace\", \"link\": \"" + url + "alsace\", \"description\": \"Welcome in Alsace\"}],");
-        sb.append("\"centre\": [{\"type\": \"locked\", \"title\": \"Centre\", \"link\": \"" + url + "alsace\", \"description\": \"Le Centre est une région française, qui regroupe six départements : le Cher, l'Eure-et-Loir, l'Indre, l'Indre-et-Loire, le Loir-et-Cher et le Loiret.\"}],");
+        sb.append("\"champagne-ardenne\": [{\"type\": \"opened\", \"title\": \"Champagne-Ardenne\", \"link\":\"" + Resources.PUBLIC_URL_MAP + Resources.PUBLIC_PARAM_MAP + "champagne-ardenne\", \"description\": \"Want to drink a glass of Champagne?\"}],");
+        sb.append("\"alsace\": [{\"type\": \"opened\", \"title\": \"Alsace\", \"link\": \"" + Resources.PUBLIC_URL_MAP + Resources.PUBLIC_PARAM_MAP + "alsace\", \"description\": \"Welcome in Alsace\"}],");
+        sb.append("\"centre\": [{\"type\": \"locked\", \"title\": \"Centre\", \"link\": \"" + Resources.PUBLIC_URL_MAP + Resources.PUBLIC_PARAM_MAP + "alsace\", \"description\": \"Le Centre est une région française, qui regroupe six départements : le Cher, l'Eure-et-Loir, l'Indre, l'Indre-et-Loire, le Loir-et-Cher et le Loiret.\"}],");
         sb.append("\"corse\": [{\"type\": \"done\", \"title\": \"Corse\", \"description\": \"Corse, ma belle Corse, que veux-tu faire par monts et par vaux...\"}],");
         sb.append("\"ile-de-france\": [{\"type\": \"done\", \"title\": \"Île de France\"}]");
         sb.append("}");
@@ -52,13 +52,13 @@ public class ServiceMap {
         StringBuilder sb = new StringBuilder();
         sb.append("{");
         if ("alsace".equals(id)) {
-            sb.append("\"bas-rhin\": [{\"type\": \"opened\", \"link\": \"\", \"description\": \"Mulhouse\"}],");
+            sb.append("\"bas-rhin\": [{\"type\": \"opened\", \"link\": \"\", \"description\": \"Strasbourg\"}],");
             sb.append("\"haut-rhin\": [{\"type\": \"opened\", \"link\": \"\", \"description\": \"Mulhouse\"}]");
         } else if ("champagne-ardenne".equals(id)) {
             sb.append("\"marne\": [{\"type\": \"closed\", \"link\": \"\", \"description\": \"Strasbourg\"}],");
             sb.append("\"aube\": [{\"type\": \"closed\", \"link\": \"\", \"description\": \"Strasbourg\"}],");
             sb.append("\"ardennes\": [{\"type\": \"opened\", \"title\": \"Combat 1v1\", \"link\": \"" + combat + FightDemo.class.getSimpleName() + "\", \"description\": \"Kill one\"}],");
-            sb.append("\"haute-marne\" : [{\"type\": \"opened\", \"title\": \"Panoply\", \"link\": \"" + pano + "active\", \"description\": \"Bla bla bla\"}]");
+            sb.append("\"haute-marne\" : [{\"type\": \"opened\", \"title\": \"Panoply\", \"link\": \"" + Resources.PUBLIC_URL_PANOPLY + Resources.PUBLIC_PARAM_PANOPLY + "active\", \"description\": \"Bla bla bla\"}]");
         } else {
             return Response.status(HttpStatus.NOT_FOUND).build();
         }
